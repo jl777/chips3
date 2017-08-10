d7e10fc51c44413bb400af2e632ba08c6bb13af6
jl777 <jameslee777@yahoo.com>
2017-08-10 17:35:06 +0200

    Test

--- src/utiltime.cpp
+++ src/utiltime.cpp
@@ -65,14 +65,14 @@ void MilliSleep(int64_t n)
  * until fixed in 1.52. Use the deprecated sleep method for the broken case.
  * See: https://svn.boost.org/trac/boost/ticket/7238
  */
-#if defined(HAVE_WORKING_BOOST_SLEEP_FOR)
-    boost::this_thread::sleep_for(boost::chrono::milliseconds(n));
-#elif defined(HAVE_WORKING_BOOST_SLEEP)
+//#if defined(HAVE_WORKING_BOOST_SLEEP_FOR)
+//    boost::this_thread::sleep_for(boost::chrono::milliseconds(n));
+//#elif defined(HAVE_WORKING_BOOST_SLEEP)
     boost::this_thread::sleep(boost::posix_time::milliseconds(n));
-#else
+//#else
 //should never get here
-#error missing boost sleep implementation
-#endif
+//#error missing boost sleep implementation
+//#endif
 }
 
 std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime)
