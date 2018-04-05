db493fc0e9457d47c2abcb4233c2c7af2059625a
jl777 <jameslee777@yahoo.com>
2018-04-05 20:11:51 +0300

    Fix

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -197,14 +197,15 @@ int32_t gettxout_scriptPubKey(uint8_t *scriptPubKey,int32_t maxsize,uint256 txid
 {
     int32_t i,m; uint8_t *ptr;
     LOCK(cs_main);
+    CBlockIndex* blockindex = nullptr;
     CTransactionRef tx;
     uint256 hashBlock;
-    if ( GetTransaction(txid,tx,Params().GetConsensus(),hashBlock,true) == 0 )
+    if ( GetTransaction(txid,tx,Params().GetConsensus(),hashBlock,true,blockindex) == 0 )
         return(-1);
-    else if ( n <= (int32_t)tx.vout.size() ) // vout.size() seems off by 1
+    else if ( n <= (int32_t)tx->vout.size() ) // vout.size() seems off by 1
     {
-        ptr = (uint8_t *)tx.vout[n].scriptPubKey.data();
-        m = tx.vout[n].scriptPubKey.size();
+        ptr = (uint8_t *)tx->vout[n].scriptPubKey.data();
+        m = tx->vout[n].scriptPubKey.size();
         for (i=0; i<maxsize&&i<m; i++)
             scriptPubKey[i] = ptr[i];
         //fprintf(stderr,"got scriptPubKey via rawtransaction\n");
