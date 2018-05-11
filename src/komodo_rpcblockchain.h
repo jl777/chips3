1f5924dfc483d5d9e1b7f1a8b47f8677a4bd9bb7
jl777 <jameslee777@yahoo.com>
2018-05-11 19:42:46 +0300

    Update

--- src/komodo_rpcblockchain.h
+++ src/komodo_rpcblockchain.h
@@ -1,10 +1,18 @@
-//
-//  komodo_rpcblockchain.h
-//  chips
-//
-//  Created by Mac on 5/11/18.
-//  Copyright © 2018 SuperNET. All rights reserved.
-//
+/******************************************************************************
+ * Copyright © 2014-2018 The SuperNET Developers.                             *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * SuperNET software, including this file may be copied, modified, propagated *
+ * or distributed except according to the terms contained in the LICENSE file *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
 
 #ifndef komodo_rpcblockchain_h
 #define komodo_rpcblockchain_h
