3f1b2eecaf30f98ecc78c37cccdca5eedb998b10
jl777 <jameslee777@yahoo.com>
2018-05-20 16:44:52 +0300

    Test

--- src/komodo_validation015.h
+++ src/komodo_validation015.h
@@ -127,7 +127,7 @@ int32_t komodo_importaddress(std::string pubkeyspend)
         std::vector<unsigned char> data(ParseHex(pubkeyspend));
         if ( !ExtractDestination(CScript(data.begin(), data.end()), address) != 0 && IsValidDestination(address) != 0 )
         {
-            printf("komodo_importaddress %s\n",EncodeDestination(address).ToString().c_str());
+            printf("komodo_importaddress %s\n",EncodeDestination(address).c_str());
             ImportAddress(pwallet, address, strLabel);
             return(0);
         }
