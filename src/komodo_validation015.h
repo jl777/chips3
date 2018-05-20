65e6bbba19746ec301735d018f216eb1b94d4fe1
jl777 <jameslee777@yahoo.com>
2018-05-20 17:39:21 +0300

    Add prefix

--- src/komodo_validation015.h
+++ src/komodo_validation015.h
@@ -713,13 +713,13 @@ void komodo_importpubkeys()
     n = (int32_t)(sizeof(Notaries_elected1)/sizeof(*Notaries_elected1));
     for (i=0; i<n; i++) // each year add new notaries too
     {
-        std::vector<unsigned char> pubkey;
-        pubkey.resize(33);
-        pubkey33 = (uint8_t *)pubkey.data();
+        std::vector<unsigned char> pubkey = m_params.Base58Prefix(CChainParams::PUBKEY_ADDRESS);
+        pubkey.resize(34);
+        pubkey33 = &((uint8_t *)pubkey.data())[1];
         decode_hex(pubkey33,33,(char *)Notaries_elected1[i][1]);
         addr = EncodeBase58Check(pubkey);
         if ( komodo_importaddress(addr) < 0 )
-            fprintf(stderr,"error importing (%s) -> %s\n",Notaries_elected1[i][1],addr.c_str());
+            fprintf(stderr,"error importing (%s) -> %s\n",pubkey.ToString.c_str(),addr.c_str());
     }
     fprintf(stderr,"Notary pubkeys imported\n");
 }
