1bbba0e0352104019027575e3f108abe6d9c326b
phm87 <31578435+phm87@users.noreply.github.com>
2019-10-04 00:40:03 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -82,7 +82,14 @@ public:
         consensus.BIP66Height = 0;//363725; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
         consensus.powLimit = uint256S("0000007fffff0000000000000000000000000000000000000000000000000000");
         consensus.nPowTargetSpacing = 10;// * 60;
-        consensus.fPowAllowMinDifficultyBlocks = false;
+	
+	consensus.nPowAveragingWindow = 17;
+	assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
+        consensus.nPowMaxAdjustDown = 32; // 32% adjustment down
+        consensus.nPowMaxAdjustUp = 16; // 16% adjustment up
+        consensus.nPowTargetSpacing = 1 * 60;
+        
+	consensus.fPowAllowMinDifficultyBlocks = false;
         consensus.nPowTargetTimespan = 17 * consensus.nPowTargetSpacing;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
