f0af475d574180c3543e2fede0804d03e43a1082
phm87 <31578435+phm87@users.noreply.github.com>
2019-10-04 00:28:49 +0200

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -73,6 +73,10 @@ struct Params {
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
+    
+    int64_t AveragingWindowTimespan() const { return nPowAveragingWindow * nPowTargetSpacing; }
+    int64_t MinActualTimespan() const { return (AveragingWindowTimespan() * (100 - nPowMaxAdjustUp  )) / 100; }
+    int64_t MaxActualTimespan() const { return (AveragingWindowTimespan() * (100 + nPowMaxAdjustDown)) / 100; }
     uint256 nMinimumChainWork;
     uint256 defaultAssumeValid;
 };
