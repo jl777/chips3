e552eea95f27462d8da53e02bcdcc14aeb02255f
phm87 <31578435+phm87@users.noreply.github.com>
2019-10-06 00:48:54 +0200

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -271,7 +271,7 @@ arith_uint256 zawy_TSA_EMA(int32_t height,int32_t tipdiff,arith_uint256 prevTarg
 
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
- printf("Current nHeight = %d nAdaptativePoWActivationThreshold = %d\n", pindexLast->nHeight, params.nAdaptativePoWActivationThreshold);
+ LogPrint("Current nHeight = %d nAdaptativePoWActivationThreshold = %d\n", pindexLast->nHeight, params.nAdaptativePoWActivationThreshold);
     if (pindexLast->nHeight < params.nAdaptativePoWActivationThreshold) {
         printf("OLD Diff algo\n");
         assert(pindexLast != nullptr);
@@ -308,7 +308,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
  }
  else {
-  printf("New Diff algo\n");
+  LogPrint("New Diff algo\n");
 //    if (ASSETCHAINS_ALGO != ASSETCHAINS_EQUIHASH && ASSETCHAINS_STAKED == 0)
      return lwmaGetNextWorkRequired(pindexLast, pblock, params);
  }
