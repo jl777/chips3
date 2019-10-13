bf5c0bfd23551bf7aaafcfbe377949069867a234
phm87 <31578435+phm87@users.noreply.github.com>
2019-10-13 15:55:46 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,7 +94,7 @@ public:
         consensus.nPowTargetTimespan = 17 * consensus.nPowTargetSpacing;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 1916; // 95% of 2016
-        consensus.nAdaptativePoWActivationThreshold = 4781925;
+        consensus.nAdaptativePoWActivationThreshold = 4794291;
         consensus.nMinerConfirmationWindow = 2016; // nPowTargetTimespan / nPowTargetSpacing
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
